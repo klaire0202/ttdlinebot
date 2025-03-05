bdab3301bea9f44e46b0d11f43e8e13f39c107c8
klaire0202 <91170773+klaire0202@users.noreply.github.com>
2025-03-05 23:45:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @line_handler.add(MemberJoinedEvent)
 def handle_member_join(event):
     new_member_id = event.joined.members[0].user_id
-    welcome_message = f"新成員你好，進來請先看記事本的群規須知。\n也可以看看記事本與相簿裡的攻略熟悉一下。\n若已有帳號，請將遊戲名片放入相簿裡。"
+    welcome_message = f"新成員你好，進來請先看記事本的群規。\n也可以看看記事本與相簿裡的攻略熟悉一下。\n若已有帳號，請將遊戲名片放入相簿裡。"
     
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=welcome_message))
 
@@ -43,39 +43,39 @@ def handle_member_join(event):
 def handle_message(event):
     user_message = event.message.text
     if "增益" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "問" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "哪個" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
     
     if "哪一個" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "抽光還是存起來" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "存起來還是抽光" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "主線還是活動" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "活動還是推主線" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "活動還是主線" in user_message:
-        reply_message = "❗請善用搜尋❗\n記事本、相簿、聊天室皆可查詢。"
+        reply_message = "請善用搜尋\n記事本、相簿、聊天室皆可查詢。"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 if __name__ == "__main__":
