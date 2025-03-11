3a2eab821829726da3f8302a139723860dfe9fe4
klaire0202 <91170773+klaire0202@users.noreply.github.com>
2025-03-11 16:59:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ def handle_message(event):
         reply_message = "台服：\n第一個異裝是「制服」\n分別有「日向、影山、研磨、黑尾、青根、二口、黃金川」\n\n第二異裝主題是「掃除」\n分別有「免費送的旭、大地、菅原、及川、岩泉」"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
-    '''if "課金" in user_message or "微課" in user_message or "儲值" in user_message or "禮包" in user_message:
+    if "課金" in user_message or "微課" in user_message or "儲值" in user_message or "禮包" in user_message:
         reply_message = "課金推薦套組\n✅每日禮包 $90\n✅通行證 $190\n✅月卡 $220\n28天總花費$2930"
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))'''
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
     if "升哪個技能" in user_message or "升什麼技能" in user_message or "升技能" in user_message or "技能升" in user_message:
         reply_message = "小幫手建議 升技能：\n🔹️銅卡：被動>托球>飛身接球>接球\n🔹️銀卡：看各個角色需求\n🔹️金卡：扣球攻擊(斜線、直線、吊球、擋出界)、托球、擋路、軟攔\n🔹️彩卡：buff、彩扣\n\n若想重製技能轉移到同角異裝也是可以，但技能要相同，顏色也要相同才可以呦！\n\n❗️重製不會返回金幣與萬能❗️會是該角色指定的技能\n\n所以要用「萬能卡」升時，必須想好哦！之後重製返回不會是萬能卡"
