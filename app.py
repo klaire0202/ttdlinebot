21131698390f823c4ea6545138d821a41fca718b
klaire0202 <91170773+klaire0202@users.noreply.github.com>
2025-03-14 13:36:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def handle_message(event):
         reply_message = "配置【增益等級】的選手\n → 把「白卡」納入上場隊伍裡，上場或候補都可以。\n❗️教練不算在內❗️"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
-    if "7777" in user_message or "吸" in user_message or "c" in user_message or "C" in user_message:
+    if user_message.lower() == "dc":
+        pass
+    elif "7777" in user_message or "吸" in user_message or "c" in user_message or "C" in user_message:
         reply_message = "小幫手眼紅中 別再曬了🥹"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
